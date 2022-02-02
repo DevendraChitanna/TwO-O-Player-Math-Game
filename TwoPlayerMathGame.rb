e7f466ca048305fee988acbe953d44240d7996c9
DevendraChitanna <devendrachitanna@gmail.com>
2022-02-02 17:50:59 -0500

    initial commit

---
+++ TwoPlayerMathGame.rb
@@ -0,0 +1,51 @@
+#Player class, Game Class, and a question class
+#game class >  player and question class
+
+#player class = takes care of lives, boolean is_alive = if life = 0
+#change is_alive to false
+#initalize with 3 lives each player
+
+#question class create 2 vars num1 and num2 and use randome num function
+#method that creates question puts what does (num1) plus (num2) equal?
+#new method finds answer to question num1 + num2
+
+
+#Game Class initalize player class with player1 and 2** @player1 = Player.new(p1)***
+#init current player   @currentPlayer = Player1, Player2
+#method called turn that Ask question, check score, should i keep playing?, ask new q or quit game.
+#create checkScore method, 
+
+
+#Create a class that creates players. Names of players?
+#class o fplayers
+#each player nees to have lives
+#a method that keeps track of players lives
+
+#current player variable -- This is used to keep track of current player
+
+#Create a method called turn that generates the question, take input
+#and compare to actual answer
+#if ans is wrong update player_lives -1
+#if right switch to other player 
+
+
+#Create a class with a function that generates random math
+#addition questions
+#create a function that takes care of lives 
+
+
+#Need to use stdin.gets.chomp to take in answers
+#Answers need to check with the current question 
+
+
+#When player? andwers switch to other player
+
+#the program only stops when a players live reaches 0/3
+#then declares the winner 
+
+
+
+#Winner function
+#if current player_lives = 0 the other player wins
+
+
